6ce4f9111bcd8056792afe315cd953e587f772f9
solistz <73733131+solistz@users.noreply.github.com>
2021-11-10 15:23:52 +0200

    mssql - connect to 10.200.0.252 user solist db db_hez - OK

--- main.py
+++ main.py
@@ -3,6 +3,25 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pyodbc
+
+def connectdb():
+    server = '10.200.0.252'
+    database = 'db_hez'
+    username = 'solist'
+    password = '123456'
+    cnxn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = cnxn.cursor()
+
+    # Sample select query
+    cursor.execute("SELECT @@version;")
+    row = cursor.fetchone()
+    print(row)
+    while row:
+        print(row[0])
+        row = cursor.fetchone()
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +31,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    connectdb()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
